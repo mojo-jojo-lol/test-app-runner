53a1cfb8b0aeb2b8c555713c45932ac4b5ad2e62
mojo-jojo-lol <110245542@users.noreply.github.com>
2023-05-08 12:40:56 +0000

    :lock: feat: allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4mylon)mstp243hqj0edj7^hs5ku)ie8vl*l03fj50+zdu(gkb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.awsapprunner.com']
 
 
 # Application definition
